2f5c31b40392045b0acf541c63b06416c1c0777b
Rachael <rachaelewins@hotmail.com>
2020-06-08 16:18:18 +0100

    Airport, has no plane by befault

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -1,9 +1,13 @@
 describe('Plane',function(){
+
   var plane;
+
   beforeEach(function(){
     plane = new Plane();
   });
+
   it('can land at an airport', function(){
     expect(plane.landAt).not.toBeUndefined()
   });
+  
 })
